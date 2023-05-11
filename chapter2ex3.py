27909c8b82cb2fce1cc110693435ee756b121a19
Mulanga <132887947+Mulanga-debug@users.noreply.github.com>
2023-05-11 10:42:44 +0300

    Add files via upload
    solutions for chapter2

---
+++ chapter2ex3.py
@@ -0,0 +1,5 @@
+//Exercise3
+time = input("Enter Hours: ")
+rate = input("Enter enter: ")
+pay = float(time) * float(rate)
+print("pay:",pay)
---
+++ chapter2ex5.py
@@ -0,0 +1,4 @@
+//Exercise5
+celsius = float(input("Enter temperature in celsius: "))
+fareneit = (celsius * 1.8) + 32
+print(str(celsius )+ "degree celsius is equal to " + str(Fahrenheit )+"degree Fahrenheit.")
