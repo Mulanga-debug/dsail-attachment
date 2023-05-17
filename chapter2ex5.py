21251521ec29cd127a43f820e506213a86322cdc
Mulanga <132887947+Mulanga-debug@users.noreply.github.com>
2023-05-17 15:33:16 +0300

    Update chapter2ex5.py

--- chapter2ex5.py
+++ chapter2ex5.py
@@ -1,4 +1,4 @@
 
 celsius = float(input("Enter temperature in celsius: "))
-fareneit = (celsius * 1.8) + 32
+Fahrenheit = (celsius * 1.8) + 32
 print(str(celsius )+ "degree celsius is equal to " + str(Fahrenheit )+"degree Fahrenheit.")
